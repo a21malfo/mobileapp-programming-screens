32b4ca27a5df83fa5f3df378a2ff428c3ccc4d41
a21malfo <a21@malfo@student.his.se>
2023-05-03 22:14:23 +0200

    Add finish to go back to MainActivity Add intent putExtra to put in data to ThirdActivity

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -2,13 +2,14 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +21,18 @@ public class SecondActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.d("SCREEN2", "goback");
                 finish();
+            }
+        });
+
+        Button goforward = findViewById(R.id.goforward);
+        goforward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("SCREEN2" , "Start ThirdActivity");
+
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                intent.putExtra("name", "GOOD CHOICE! ALWAYS GO FORWARD!");
+                startActivity(intent);
             }
         });
     }
